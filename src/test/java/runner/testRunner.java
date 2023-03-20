ebf5e0d3617611b4b8dd93b87ae29048fab05fd1
Akansha <akansha.gupta.ext@siemens-energy.com>
2023-03-20 17:10:11 +0530

    added testrunner class

---
+++ src/test/java/runner/testRunner.java
@@ -0,0 +1,21 @@
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features = "src/com/aires/features/"
+		,glue = {"stepDefinitions"}
+		//,dryRun = true
+		//,tags = {"@SmokeTest, @Regression"}
+		,tags = {"@me"}
+	//	,tags = (CoreConstants.TAG_VALUE)
+		,plugin = { "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:", "junit:target/cucumber-results.xml", "html:target/cucumber-results", "json:target/cucumber-results.json"},
+		monochrome = true
+		)
+
+public class testRunner { }
