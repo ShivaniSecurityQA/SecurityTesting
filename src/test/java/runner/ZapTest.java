b8b83dc515ebe5035bdfd484a18e1d5f0e7aef8b
Akansha <akansha.gupta.ext@siemens-energy.com>
2023-03-21 16:28:14 +0530

    Added Zap class

--- src/test/java/runner/ZapTest.java
+++ src/test/java/runner/ZapTest.java
@@ -1,21 +1,49 @@
 package runner;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.zaproxy.clientapi.core.ClientApiException;
+
+import commonUtilities.ZapUtility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
+
 public class ZapTest {
-	WebDriver driver;
-	ChromeOptions coptions= new ChromeOptions();
-	coptions.setProxy(ZapUtility.proxy);
 	
-	WebDriverManager.chromedriver().setup();
-	driver= new ChromeDriver(coptions);
+
+		public void test() throws ClientApiException {
+			WebDriver driver;
+			
+			ChromeOptions test =new ChromeOptions();
+			//FirefoxOptions fo = new FirefoxOptions();
+			//WebDriverManager.chromedriver();
+			//	driver= new ChromeDriver(coptions);
+				
+				//generating zap report
+				ZapUtility.zapReport();
+			test.setProxy(ZapUtility.proxy);
+			WebDriverManager.chromedriver();
+			driver= new ChromeDriver(coptions);
+		}
+
+}	
+	//WebDriverManager.chromedriver();
+	//driver= new ChromeDriver(coptions);
 	
 	//generating zap report
-	ZapUtility.zapReport();
+	//ZapUtility.zapReport();
 	
 	
 	
@@ -28,4 +56,5 @@ public class ZapTest {
 //	
 //	
 
-}
+
+	
